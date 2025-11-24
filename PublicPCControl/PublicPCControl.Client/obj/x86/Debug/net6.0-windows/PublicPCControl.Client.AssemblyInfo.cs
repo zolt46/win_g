df7b28e37534164b121184b9ed8604c729fc38eb
zolt46 <emanon108@svk.jp>
2025-11-24 20:44:28 +0900

    win guard

--- PublicPCControl/PublicPCControl.Client/obj/x86/Debug/net6.0-windows/PublicPCControl.Client.AssemblyInfo.cs
+++ PublicPCControl/PublicPCControl.Client/obj/x86/Debug/net6.0-windows/PublicPCControl.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublicPCControl.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccd75ed3d8ac5de645c7376613c26ca2b8a8a311")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6511697ed03f1e80f1c2abdb126d7fc93b39158")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublicPCControl.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PublicPCControl.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
