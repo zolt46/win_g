62fccdb880a3144f4aee6215e94aa43e8245c932
zolt46 <seewoopark46@gmail.com>
2025-11-27 15:51:55 +0900

    Fix XAML container and combo box build errors

---
+++ PublicPCControl/PublicPCControl.Client/Services/ShortcutResolver.cs
@@ -0,0 +1,109 @@
+// File: PublicPCControl.Client/Services/ShortcutResolver.cs
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace PublicPCControl.Client.Services
+{
+    public static class ShortcutResolver
+    {
+        public static string? ResolveTargetPath(string shortcutPath)
+        {
+            if (string.IsNullOrWhiteSpace(shortcutPath) || !File.Exists(shortcutPath))
+            {
+                return null;
+            }
+
+            try
+            {
+                var link = (IShellLinkW)new ShellLink();
+                var persistFile = (IPersistFile)link;
+                persistFile.Load(shortcutPath, STGM_READ);
+
+                var builder = new StringBuilder(260);
+                var data = new WIN32_FIND_DATAW();
+                link.GetPath(builder, builder.Capacity, out data, SLGP_RAWPATH);
+
+                var target = builder.ToString();
+                return string.IsNullOrWhiteSpace(target) ? null : target;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private const uint STGM_READ = 0x00000000;
+        private const uint SLGP_RAWPATH = 0x00000004;
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+        private struct WIN32_FIND_DATAW
+        {
+            public uint dwFileAttributes;
+            public FILETIME ftCreationTime;
+            public FILETIME ftLastAccessTime;
+            public FILETIME ftLastWriteTime;
+            public uint nFileSizeHigh;
+            public uint nFileSizeLow;
+            public uint dwReserved0;
+            public uint dwReserved1;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string cFileName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 14)]
+            public string cAlternateFileName;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct FILETIME
+        {
+            public uint dwLowDateTime;
+            public uint dwHighDateTime;
+        }
+
+        [ComImport]
+        [Guid("000214F9-0000-0000-C000-000000000046")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        private interface IShellLinkW
+        {
+            void GetPath([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszFile, int cchMaxPath, out WIN32_FIND_DATAW pfd, uint fFlags);
+            void GetIDList(out IntPtr ppidl);
+            void SetIDList(IntPtr pidl);
+            void GetDescription([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszName, int cchMaxName);
+            void SetDescription([MarshalAs(UnmanagedType.LPWStr)] string pszName);
+            void GetWorkingDirectory([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszDir, int cchMaxPath);
+            void SetWorkingDirectory([MarshalAs(UnmanagedType.LPWStr)] string pszDir);
+            void GetArguments([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszArgs, int cchMaxPath);
+            void SetArguments([MarshalAs(UnmanagedType.LPWStr)] string pszArgs);
+            void GetHotkey(out short pwHotkey);
+            void SetHotkey(short wHotkey);
+            void GetShowCmd(out int piShowCmd);
+            void SetShowCmd(int iShowCmd);
+            void GetIconLocation([Out, MarshalAs(UnmanagedType.LPWStr)] StringBuilder pszIconPath, int cchIconPath, out int piIcon);
+            void SetIconLocation([MarshalAs(UnmanagedType.LPWStr)] string pszIconPath, int iIcon);
+            void SetRelativePath([MarshalAs(UnmanagedType.LPWStr)] string pszPathRel, uint dwReserved);
+            void Resolve(IntPtr hwnd, uint fFlags);
+            void SetPath([MarshalAs(UnmanagedType.LPWStr)] string pszFile);
+        }
+
+        [ComImport]
+        [Guid("00021401-0000-0000-C000-000000000046")]
+        private class ShellLink
+        {
+        }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("0000010b-0000-0000-C000-000000000046")]
+        private interface IPersistFile
+        {
+            void GetClassID(out Guid pClassID);
+            [PreserveSig]
+            int IsDirty();
+            void Load([MarshalAs(UnmanagedType.LPWStr)] string pszFileName, uint dwMode);
+            void Save([MarshalAs(UnmanagedType.LPWStr)] string pszFileName, [MarshalAs(UnmanagedType.Bool)] bool fRemember);
+            void SaveCompleted([MarshalAs(UnmanagedType.LPWStr)] string pszFileName);
+            void GetCurFile([MarshalAs(UnmanagedType.LPWStr)] out string ppszFileName);
+        }
+    }
+}
--- PublicPCControl/PublicPCControl.Client/ViewModels/AdminViewModel.cs
+++ PublicPCControl/PublicPCControl.Client/ViewModels/AdminViewModel.cs
@@ -1,8 +1,10 @@
 // File: PublicPCControl.Client/ViewModels/AdminViewModel.cs
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 using PublicPCControl.Client.Models;
 using PublicPCControl.Client.Services;
@@ -20,8 +22,14 @@ namespace PublicPCControl.Client.ViewModels
         private AppConfig _config = new();
         private string _newProgramName = string.Empty;
         private string _newProgramPath = string.Empty;
+        private string _newProgramArguments = string.Empty;
+        private string _programSearchText = string.Empty;
 
         public ObservableCollection<AllowedProgram> AllowedPrograms { get; } = new();
+        public ObservableCollection<ProgramSuggestion> ProgramSuggestions { get; } = new();
+
+        public ICollectionView AllowedProgramsView { get; }
+        public ICollectionView ProgramSuggestionsView { get; }
 
         public bool EnforcementEnabled
         {
@@ -32,6 +40,11 @@ namespace PublicPCControl.Client.ViewModels
                 if (value)
                 {
                     _config.IsAdminOnlyPc = false;
+                    OnPropertyChanged(nameof(IsAdminOnlyPc));
+                }
+                else if (!_config.IsAdminOnlyPc)
+                {
+                    _config.IsAdminOnlyPc = true;
                     OnPropertyChanged(nameof(IsAdminOnlyPc));
                 }
                 OnPropertyChanged();
@@ -47,6 +60,11 @@ namespace PublicPCControl.Client.ViewModels
                 if (value)
                 {
                     _config.EnforcementEnabled = false;
+                    OnPropertyChanged(nameof(EnforcementEnabled));
+                }
+                else if (!_config.EnforcementEnabled)
+                {
+                    _config.EnforcementEnabled = true;
                     OnPropertyChanged(nameof(EnforcementEnabled));
                 }
                 OnPropertyChanged();
@@ -107,12 +125,33 @@ namespace PublicPCControl.Client.ViewModels
             }
         }
 
+        public string NewProgramArguments
+        {
+            get => _newProgramArguments;
+            set => SetProperty(ref _newProgramArguments, value);
+        }
+
+        public string ProgramSearchText
+        {
+            get => _programSearchText;
+            set
+            {
+                if (SetProperty(ref _programSearchText, value))
+                {
+                    AllowedProgramsView.Refresh();
+                    ProgramSuggestionsView.Refresh();
+                }
+            }
+        }
+
         public ICommand AddProgramCommand { get; }
         public ICommand RemoveProgramCommand { get; }
         public ICommand SaveCommand { get; }
         public ICommand CloseCommand { get; }
         public ICommand EnterMaintenanceCommand { get; }
         public ICommand ResumeFromMaintenanceCommand { get; }
+        public ICommand UseSuggestionCommand { get; }
+        public ICommand RefreshSuggestionsCommand { get; }
 
         public AdminViewModel(
             ConfigService configService,
@@ -134,6 +173,15 @@ namespace PublicPCControl.Client.ViewModels
             CloseCommand = new RelayCommand(_ => _close());
             EnterMaintenanceCommand = new RelayCommand(_ => _enterMaintenance());
             ResumeFromMaintenanceCommand = new RelayCommand(_ => _resumeFromMaintenance(), _ => _isMaintenanceActive());
+            RefreshSuggestionsCommand = new RelayCommand(_ => LoadProgramSuggestions());
+
+            AllowedProgramsView = CollectionViewSource.GetDefaultView(AllowedPrograms);
+            AllowedProgramsView.Filter = FilterPrograms;
+
+            ProgramSuggestionsView = CollectionViewSource.GetDefaultView(ProgramSuggestions);
+            ProgramSuggestionsView.Filter = FilterSuggestions;
+
+            UseSuggestionCommand = new RelayCommand(p => ApplySuggestion(p as ProgramSuggestion), p => p is ProgramSuggestion);
         }
 
         public void Refresh(AppConfig config)
@@ -142,9 +190,12 @@ namespace PublicPCControl.Client.ViewModels
             AllowedPrograms.Clear();
             foreach (var program in _config.AllowedPrograms)
             {
+                program.Icon ??= IconHelper.LoadIcon(program.ExecutablePath);
                 AllowedPrograms.Add(program);
             }
             OnPropertyChanged(string.Empty);
+            EnsureModeSelected();
+            LoadProgramSuggestions();
             if (ResumeFromMaintenanceCommand is RelayCommand relay)
             {
                 relay.RaiseCanExecuteChanged();
@@ -173,11 +224,13 @@ namespace PublicPCControl.Client.ViewModels
                 return;
             }
 
-            var program = new AllowedProgram { DisplayName = NewProgramName, ExecutablePath = NewProgramPath };
+            var program = new AllowedProgram { DisplayName = NewProgramName, ExecutablePath = NewProgramPath, Arguments = NewProgramArguments };
+            program.Icon ??= IconHelper.LoadIcon(program.ExecutablePath);
             AllowedPrograms.Add(program);
             _config.AllowedPrograms = AllowedPrograms.ToList();
             NewProgramName = string.Empty;
             NewProgramPath = string.Empty;
+            NewProgramArguments = string.Empty;
         }
 
         public void Save()
@@ -213,11 +266,73 @@ namespace PublicPCControl.Client.ViewModels
             _config.AllowedPrograms = AllowedPrograms.ToList();
         }
 
+        private void ApplySuggestion(ProgramSuggestion? suggestion)
+        {
+            if (suggestion == null)
+            {
+                return;
+            }
+
+            NewProgramName = suggestion.DisplayName;
+            NewProgramPath = suggestion.ExecutablePath;
+            AddProgram();
+        }
+
         private void RefreshProgramCommands()
         {
             if (AddProgramCommand is RelayCommand relay)
             {
                 relay.RaiseCanExecuteChanged();
+            }
+        }
+
+        private void LoadProgramSuggestions()
+        {
+            ProgramSuggestions.Clear();
+            foreach (var suggestion in ProgramDiscoveryService.FindSuggestions())
+            {
+                ProgramSuggestions.Add(suggestion);
+            }
+            ProgramSuggestionsView.Refresh();
+        }
+
+        private bool FilterPrograms(object obj)
+        {
+            if (obj is not AllowedProgram program)
+            {
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(ProgramSearchText))
+            {
+                return true;
+            }
+
+            return program.DisplayName.Contains(ProgramSearchText, System.StringComparison.OrdinalIgnoreCase)
+                   || program.ExecutablePath.Contains(ProgramSearchText, System.StringComparison.OrdinalIgnoreCase);
+        }
+
+        private bool FilterSuggestions(object obj)
+        {
+            if (obj is not ProgramSuggestion suggestion)
+            {
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(ProgramSearchText))
+            {
+                return true;
+            }
+
+            return suggestion.DisplayName.Contains(ProgramSearchText, System.StringComparison.OrdinalIgnoreCase);
+        }
+
+        private void EnsureModeSelected()
+        {
+            if (!_config.EnforcementEnabled && !_config.IsAdminOnlyPc)
+            {
+                _config.EnforcementEnabled = true;
+                OnPropertyChanged(nameof(EnforcementEnabled));
             }
         }
     }
