967aa4a4b72fec45eab29c52d3ecddd62e4639b9
zolt46 <seewoopark46@gmail.com>
2025-12-02 14:27:55 +0900

    latest ver

--- PublicPCControl/PublicPCControl.Client/obj/x86/Debug/net8.0-windows/PublicPCControl.Client.AssemblyInfo.cs
+++ PublicPCControl/PublicPCControl.Client/obj/x86/Debug/net8.0-windows/PublicPCControl.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PublicPCControl.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f518de15fb27553dda4130df3b5b700b3f97c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db6a5c1d052d0b4cc5224af390a1fb4ca39b2385")]
 [assembly: System.Reflection.AssemblyProductAttribute("PublicPCControl.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PublicPCControl.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
