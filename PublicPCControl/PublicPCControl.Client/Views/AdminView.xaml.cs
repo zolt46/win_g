7503f8332be4873e972678408941737ccf0b609b
zolt46 <seewoopark46@gmail.com>
2025-12-04 13:33:53 +0900

    Fix ProgramSuggestionsWindow build and metadata

--- PublicPCControl/PublicPCControl.Client/Views/AdminView.xaml.cs
+++ PublicPCControl/PublicPCControl.Client/Views/AdminView.xaml.cs
@@ -4,11 +4,14 @@ using System.Windows;
 using System.Windows.Controls;
 using Microsoft.Win32;
 using PublicPCControl.Client.ViewModels;
+using PublicPCControl.Client.Views;
 
 namespace PublicPCControl.Client.Views
 {
     public partial class AdminView : UserControl
     {
+        private ProgramSuggestionsWindow? _suggestionsWindow;
+
         public AdminView()
         {
             InitializeComponent();
@@ -54,6 +57,24 @@ namespace PublicPCControl.Client.Views
                 vm.ApplyNewAdminPassword(dialog.NewPassword);
                 MessageBox.Show("관리자 비밀번호가 변경되었습니다.", "비밀번호 변경", MessageBoxButton.OK, MessageBoxImage.Information);
             }
+        }
+
+        private void OnOpenSuggestions(object sender, RoutedEventArgs e)
+        {
+            if (_suggestionsWindow != null && _suggestionsWindow.IsVisible)
+            {
+                _suggestionsWindow.Activate();
+                return;
+            }
+
+            _suggestionsWindow = new ProgramSuggestionsWindow
+            {
+                Owner = Window.GetWindow(this),
+                DataContext = DataContext
+            };
+
+            _suggestionsWindow.Closed += (_, _) => _suggestionsWindow = null;
+            _suggestionsWindow.Show();
         }
     }
 }
